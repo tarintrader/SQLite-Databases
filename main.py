e05ad26fe61130dee2dea4ae42c4fe99ba2b8802
Adrian <tarintrader@gmail.com>
2024-02-21 19:43:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+
+db.init_app(app)
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+book = Books(
+    id=1,
+    title="Harry Potter",
+    author="J. K. Rowling",
+    rating=9.3
+)
+
+with app.app_context():
+    db.session.add(book)
+    db.session.commit()
